c871109510ec70006927d46a1a1579772ded2484
Nicolas Kogus <nicolaskogux67@gmail.com>
2026-04-22 12:06:14 -0300

    Foram adicionados os métodos excluirPorID, atualizarProduto e listarProdutos

--- src/main/java/com/kogusapi/produtosAPI/Produtos/controller/ProdutoController.java
+++ src/main/java/com/kogusapi/produtosAPI/Produtos/controller/ProdutoController.java
@@ -5,6 +5,8 @@ import com.kogusapi.produtosAPI.Produtos.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
@@ -13,10 +15,20 @@ public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
     @PostMapping("/salvar-produto")
-    public Produto saveProduct(@RequestBody Produto produto) {
-        return produtoRepository.save(produto);
-    }
+    public Produto salvarProduto(@RequestBody Produto produto) {return produtoRepository.save(produto);}
 
     @GetMapping("/{id}")
     public Produto pegarPorId(@PathVariable("id") Long id) {return produtoRepository.findById(id).orElse(null);}
+
+    @DeleteMapping("/{id}")
+    public void excluirPorID(@PathVariable("id") Long id) {produtoRepository.deleteById(id);}
+
+    @PutMapping("/{id}")
+    public void atualizarProduto(@PathVariable("id") Long id, @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping("/listar-produtos")
+    public List<Produto> listarProdutos() {return produtoRepository.findAll();}
 }
